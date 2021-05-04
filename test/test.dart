30641fbcf77c432b432b480db82b2a7e23ba104a
Romualdo Duarte <romualdoduarte@Mac-mini-de-Romualdo.local>
2021-05-04 14:05:27 -0300

    test: add test to LocalStorage (READ and WRITE method)

--- test/test.dart
+++ test/test.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:se_challenge/app/infra/adapters/httpClient/http_client.dart';
+import 'package:se_challenge/app/infra/adapters/localStorage/local_storage.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('ensure HttpClient Adapter is working',
-      () async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  test('ensure HttpClient Adapter is working', () async {
     final HttpClient httpClient = HttpClient();
 
     var response = await httpClient
@@ -11,11 +15,29 @@ void main() {
             url: 'https://api.github.com/search/users?q=romualdo',
             method: 'GET')
         .then((value) {
-
       return value;
     });
 
-
     expect(response.statusCode, 200);
+  });
+
+  test('ensure LocalStorage Adapter is working READ method', () async {
+    final LocalStorage storage = LocalStorage();
+
+    var response = await storage.request(method: 'READ', prop: 'test');
+
+    expect(response, null);
+  });
+
+  test('ensure LocalStorage Adapter is working WRITE method', () async {
+    final LocalStorage storage = LocalStorage();
+
+    var response = await storage
+        .request(method: 'WRITE', prop: 'test', data: 'Isto é um teste')
+        .then((value) {
+      return value;
+    });
+
+    expect(response, null);
   });
 }
